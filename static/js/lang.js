5f5c9a9d5d08c7665d5e877ad179368c75c7888a
Eddy <edwardyue93@gmail.com>
2023-12-22 08:37:58 +0100

    Se traduce la web al alemán y al francés

--- static/js/lang.js
+++ static/js/lang.js
@@ -12,7 +12,22 @@ langRadios.forEach((radio) => {
     if (e.target.value === actualLang) {
       return
     }
-    const path = window.location.pathname.split('/').at(-1)
+    const fullPath = window.location.pathname
+    const path = fullPath.split('/').at(-1)
+
+    if (
+      fullPath === '/' ||
+      fullPath === '/en' ||
+      fullPath === '/de' ||
+      fullPath === '/fr'
+    ) {
+      if (e.target.value === 'es') {
+        window.location.assign('/')
+      } else {
+        window.location.assign(`/${e.target.value}`)
+      }
+      return
+    }
 
     if (e.target.value === 'es') {
       window.location.assign(`/${path}`)
